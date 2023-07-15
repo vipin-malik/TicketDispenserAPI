a1d70b5b0440f966a5ac5da2f56da370cc4586d1
malikvipin <vipinmalik43@gmail.com>
2023-07-15 18:06:30 +0530

    Unit Test cases for the Ticket Service

--- TicketDispenserAPI.Tests/TicketServiceTests.cs
+++ TicketDispenserAPI.Tests/TicketServiceTests.cs
@@ -1,8 +1,46 @@
+using Moq;
+using TicketDispenserAPI.Repositories;
+using TicketDispenserAPI.Services;
+using Xunit;
+
 namespace TicketDispenserAPI.Tests
 {
     public class TicketServiceTests
     {
-       
+        private Mock<ITicketNumberRepository> _mockTicketNumberRepository;
+        private TicketService _ticketService;
+
+        public TicketServiceTests()
+        {
+            _mockTicketNumberRepository = new Mock<ITicketNumberRepository>();
+            _ticketService = new TicketService(_mockTicketNumberRepository.Object);
+        }
+
+        [Fact]
+        public void GetTicketNumber_ReturnsUniqueNumber()
+        {
+            // mock setup
+            _mockTicketNumberRepository.Setup(r => r.GetNextTicketNumber()).Returns(1);
+
+            // execute method to test
+            int ticketNumber = _ticketService.GetTicketNumber();
+
+            // assert results
+            Assert.Equal(1, ticketNumber);
+        }
+
+        [Fact]
+        public void GetTicketNumber_ThrowsExceptionForDuplicateTicketNumber()
+        {
+            // mock setup
+            _mockTicketNumberRepository.Setup(r => r.GetNextTicketNumber()).Returns(1);
+
+            // execute method to test
+            _ticketService.GetTicketNumber();
+
+            // assert results
+            Assert.Throws<InvalidOperationException>(() => _ticketService.GetTicketNumber());
+        }
         
     }
 }
